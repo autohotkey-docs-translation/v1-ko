9ecc8b6ed802cabaf32ece85548118c3dfc96155
Joe DF <joedf@live.ca>
2020-01-21 10:54:14 -0500

    add original translation by  johnsonj 151111 (ahk v1.1.22.07)

---
+++ src/docs/static/source/data_translate.js
@@ -0,0 +1,14 @@
+translate = {
+  hdSearchTxt: "검색할 단어를 입력하십시오 ...",
+  hdSearchBtn: "검색",
+  hdSearchLnk: "http://www.google.com/cse?cx=010629462602499112316:ywoq_rufgic&q={0}",
+  sbContent: "목차",
+  sbIndex: "색인",
+  ftLicense: "라이선스:",
+  ftExtra: "",
+  cdSelectBtn: "선택",
+  cdDownloadBtn: "내려받기",
+  verToolTipAHK_L: "다음에 적용됩니다:\nAutoHotkey_L Revision {0} 이후\nAutoHotkey v1.0.90.00 이후",
+  verToolTipDefault: "AutoHotkey {0} 이후에 적동됩니다",
+  tutLocalMessage: "이 문서를 하드에서 보고 있기 때문에, 아마도 이미 AutoHotkey가 설치되어 있을 것이고 섹션 b로 건너 뛰어도 됩니다."
+};
